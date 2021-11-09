9f1e268d1502aac5cd934d9d211ea5ac447454dc
RimoChan <RimoChan@stu.xmu.edu.cn>
2021-11-09 22:03:24 +0800

    好！

--- rimo_utils/cache.py
+++ rimo_utils/cache.py
@@ -1,9 +1,25 @@
+import json
 import pickle
 import hashlib
 from pathlib import Path
 
 
-def disk_cache(path):
+协议 = {
+    'pickle': [
+        pickle.dumps,
+        pickle.loads,
+        'pkl',
+    ],
+    'json': [
+        lambda x: json.dumps(x, sort_keys=True, ensure_ascii=False, separators=(',', ':')).encode('utf8'),
+        json.loads,
+        'json',
+    ],
+}
+
+
+def disk_cache(path, protocol='pickle'):
+    dump, load, ext = 协议[protocol]
     path = Path(path)
     if path.is_file():
         raise Exception('你不对劲')
@@ -11,16 +27,18 @@ def disk_cache(path):
     def q(func):
         name = func.__name__
         def 假func(*li, **d):
-            s = pickle.dumps([name, li, d])
-            md5 = hashlib.md5(s).hexdigest()
-            名字 = f'{name}_{md5}'
+            i = [name, li, d]
+            md5 = hashlib.md5(dump(i)).hexdigest()
+            名字 = f'{name}_{md5}.{ext}'
             if (path/名字).is_file():
                 with open(path/名字, 'rb') as f:
-                    return pickle.loads(f.read())
+                    i, o = load(f.read())
+                    return o
             else:
-                r = func(*li, **d)
+                o = func(*li, **d)
+                s = dump([i, o])
                 with open(path/名字, 'wb') as f:
-                    f.write(pickle.dumps(r))
-                return r
+                    f.write(s)
+                return o
         return 假func
     return q
